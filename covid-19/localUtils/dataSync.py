ab54a7fde328df017551c928e5a96228af676563
YE TIAN <solaris.navi@gmail.com>
2020-03-18 20:37:36 +0100

    update data

--- covid-19/localUtils/dataSync.py
+++ covid-19/localUtils/dataSync.py
@@ -119,7 +119,8 @@ missing_population_map = {
     "Südwestpfalz": 95113,
     "Tier-Saarburg": 147833,
     "Ostprignitz-Ruppin": 99078,
-    "Uckermark": 120878
+    "Uckermark": 120878,
+    "Rhön-Grabfeld": 79690
 }
 
 def loader(data, keyword):
